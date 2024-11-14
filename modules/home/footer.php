3c18650f873005a8cae8261f16bc79c3c688f5c4
LyDuong <lytrieuduong0310@gmail.com>
2024-11-14 23:55:08 +0700

    thêm footer

---
+++ modules/home/footer.php
@@ -0,0 +1,35 @@
+<footer>
+        <div class="footer-container">
+            <div class="footer-content">
+                <h3>Movie Insight</h3>
+                <p>A website offering the latest, high-quality reviews of the hottest movies with fast updates.</p>
+            </div>
+    
+ 
+            <div class="footer-links">
+                <h4>Links</h4>
+                <ul>
+                    <li><a href="#home">Home</a></li>
+                    <li><a href="#movies">Movies</a></li>
+                    <li><a href="#series">TV Shows</a></li>
+                    <li><a href="#about">About us</a></li>
+                    <li><a href="#contact">Contact</a></li>
+                </ul>
+            </div>
+            <div class="social-links">
+                <h4>Follow us</h4>
+                <div class="social-icons">
+                    <a href="https://facebook.com" target="_blank" class="facebook"><i class="fab fa-facebook-f"></i></a>
+                    <a href="https://instagram.com" target="_blank" class="instagram"><i class="fab fa-instagram"></i></a>
+                    <a href="https://twitter.com" target="_blank" class="twitter"><i class="fab fa-twitter"></i></a>
+                    <a href="https://youtube.com" target="_blank" class="youtube"><i class="fab fa-youtube"></i></a>
+                </div>
+            </div>
+        </div>
+    
+        <hr>
+    
+        <div class="copyright">© 2024 Movie Insight. All rights reserved.</p>
+        </div>
+    </footer>
+    
